e4bf5715533e61d829cb5d0b92c91f419d59eb11
Pietro Fantini <136325329+pietro-fantini@users.noreply.github.com>
2024-01-29 22:51:21 +0000

    Create proprieties.m

---
+++ proprieties.m
@@ -0,0 +1,35 @@
+% Cable05_VELDTUI2
+hmean=mean(h(25:60));
+w_mean=mean(w(25:60));
+intervallo 13.5313 - 33.0625
+natural freq = 1.7916
+damping ratio = 0.0048
+input tension = 470e3
+tension = 486e3
+
+% Cable06_SPANTUI1
+hmean=mean(h(31:66));
+w_mean=mean(w(31:66));
+intervallo 16.2031 - 34.9766
+natural freq = 1.8637
+damping ratio = 0.0051
+input tension = 705e3
+tension = 550e3
+
+% Cable08_VELDTUI1
+hmean=mean(h(42:75));
+w_mean=mean(w(42:75));
+intervallo = 22.7813 - 34.9766
+natural freq = 1.8255
+damping ratio = 0.0049
+input tension = 470e3
+tension = 525e3
+
+% Cable13_SPANTUI2
+hmean=mean(h(41:68));
+w_mean=mean(w(41:68));
+intervallo = 19.875 - 33.2188
+natural freq = 2.0226
+damping ratio = 0.0053
+input tension = 705e3
+tension = 655e3
